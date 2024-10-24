549a6d219543984caa599e6bda43434f277c91d7
sreyareddy1902 <158560047+sreyareddy1902@users.noreply.github.com>
2024-10-24 13:07:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+
+//var
+/*---
+var a    //declaration is possible
+a=10     //initialization is possible
+console.log(a)
+
+var a    //re-declaration is possible
+a=20     //re-initialization is possible
+console.log(a)
+
+var a=30   //declaration and initialization is possible
+console.log(a)
+
+var a=40   //re-declaration and re-initiazation is possible
+console.log(a)
+----*/
+
+//let
+/*---
+let a    //declaration is possible
+a=10     //initialization is possible
+console.log(a)
+
+let a //re-declaration is not possible
+a=20  //re-intilization is possible
+console.log(a)
+
+let a=30  //declaration and initialization is possible
+console.log(a)
+
+let a=40   //re-declaration and re-initialization is not possible
+-----*/
+
+
+//const
+/*--
+const x   //declaration is not possible
+x=10      //initialization is not possible
+
+const x=10 //declaration and initialization is  possible
+console.log(x)
+
+const x=20  //re-declaration and re-initialization is not possible
+----*/
+
+
+//rules for constructing variable
+
+let a=10;
+console.log(a)
+
+let myName=20
+console.log(myName)
+
+let myname=30
+console.log(myname) 
+
+//let my Name=40 //not possible
+//console.log(my Name)
+
+let my_name=40
+console.log(my_name)
+
+//let 1x=20  //digit is not possible before the naming
+//console.log(1x)
+
+let x1=20
+console.log(x1)
+
+let _a=50
+console.log(_a)
+
+let $a=60
+console.log($a)
